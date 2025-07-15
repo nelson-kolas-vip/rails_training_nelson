d35d22554da6269b54ed9ccd9282fca34206dc28
nelson-kolas-vip <nelson.kolas@vtinfo.com>
2025-07-15 09:29:14 +0000

    Added more test cases

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      extend Apipie::DSL::Concern
+
+      def_param_group :user do
+        property :id, Integer, desc: 'User ID'
+        property :first_name, String, desc: 'First name of the user'
+        property :last_name, String, desc: 'Last name of the user'
+        property :email, String, desc: 'User email'
+        property :created_at, String, desc: 'Account creation date'
+      end
+
+      api :GET, '/api/v1/users', 'Get all available users'
+      returns array_of: :user, code: 200, desc: 'List of All Users'
+      def index
+        users = User.all
+        render json: users, each_serializer: UserSerializer
+      end
+    end
+  end
+end
--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "User Sign In", type: :feature do
-  let!(:user) { create(:user, password: "secure123") }
+  let!(:user) { create(:user, password: "rails@123") }
 
   it "logs in with correct credentials" do
-    visit sign_in_path
+    visit new_user_session_path
 
     fill_in "Email", with: user.email
-    fill_in "Password", with: "secure123"
+    fill_in "Password", with: "rails@123"
 
     click_button "Sign In"
 
-    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Welcome back,")
   end
 
   it "shows error with incorrect credentials" do
-    visit sign_in_path
+    visit new_user_session_path
 
     fill_in "Email", with: user.email
     fill_in "Password", with: "wrongpass"
 
     click_button "Sign In"
 
-    expect(page).to have_content("Invalid email or password")
+    expect(page).to have_content(/Invalid email or password/i)
   end
 end
