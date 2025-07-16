7f031a63b125e8f8c349b8649aacb8c33fb996fc
nelson-kolas-vip <nelson.kolas@vtinfo.com>
2025-07-16 05:05:10 +0000

    OS-65-API-CREATE Completed

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # mount Apipie::Engine => '/apipie'
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
+      resources :users, only: [:index, :create]
     end
   end
 
