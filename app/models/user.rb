eb02c44c6f2599264ca771a72c688993baad2a30
nelson-kolas-vip <nelson.kolas@vtinfo.com>
2025-07-31 10:03:36 +0000

    Admin Section Refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
   has_many :restaurants, dependent: :destroy
+
+  enum :role_type, { admin: 1, staff: 2, customer: 3 }
+  enum :status, { active: 1, inactive: 2 }
+  def self.role_type_to_string(value)
+    role_types.key(value)
+  end
 end
