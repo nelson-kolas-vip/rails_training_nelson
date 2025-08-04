d1c10e8f8701f7773295c038e2e50e4052d06546
nelson-kolas-vip <nelson.kolas@vtinfo.com>
2025-08-04 05:16:04 +0000

    OS-78-MENU-MGT Completed

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :menu do
-    item_name { "MyString" }
-    description { "MyText" }
-    price { "9.99" }
-    category { "MyString" }
-    available { false }
-    restaurant { nil }
+    item_name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Commerce.price(range: 5.0..50.0) }
+    category { Faker::Food.ethnic_category }
+    available { [true, false].sample }
+    veg_status { Menu.veg_statuses.keys.sample }
+    association :restaurant
+    association :user
   end
 end
